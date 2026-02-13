2c3418b40935bebf0e546b64eb42c50b9bea5fec
Jawad Dergham <jdergham@umich.edu>
2026-02-12 23:23:43 -0500

    Lab 4 changes new branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("Added print\n");
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World..."+i);
         }
